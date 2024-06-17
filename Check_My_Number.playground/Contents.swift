5df925f3955579da7657a7b721fecb6777652dc5
Mabende <belindamabende@gmail.com>
2024-06-17 04:56:51 +0200

    Initial Commit

---
+++ Check_My_Number.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var myNumber = "even"
+
+switch myNumber{
+case "odd":
+    print("Odd number!")
+case "even":
+    print("Even number!")
+default:
+    print("Enter a number!")
+    
+}
